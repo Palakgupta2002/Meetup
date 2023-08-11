7238ed8b07ce880565726f4e1a4f264e9178bf76
Palakgupta2002 <pg948303@gmail.com>
2023-08-11 14:28:16 +0530

    Process

--- backend/index.js
+++ backend/index.js
@@ -21,26 +21,48 @@ const userSchema = new mongoose.Schema({
 // Create model using schema
 const User = mongoose.model('user', userSchema);
 
+//This is for to connect a two localhost to each other like gateway
 server.use(cors());
 server.use(bodyParser.json());
 server.get('/',(res,req)=>{
   req.send("hello");
 })
 
+//This is for Signup
 server.post('/Signup', async (req, res) => {
   try {
     let newUser = new User();
     newUser.username = req.body.username;
     newUser.password = req.body.password;
     const doc = await newUser.save();
-    console.log(doc);
-    console.log(req.body);
     res.json(req.body);
+  
   } catch (error) {
     console.error("Error inserting user data:", error);
     res.status(500).json({ error: "An error occurred" });
   }
 });
+//This is for Login
+// This is for Login
+server.post('/Login', async (req, res) => {
+  try {
+    const { lusername, lpassword } = req.body;
+
+    // Find the user with the provided username
+    const user = await User.findOne({ username: lusername });
+
+    // Check if the user exists and the password matches
+    if (user && user.password === lpassword) {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 
 
 //This is default
